6df12fa8d6dc3657af1cac819d75b42eea5b6b11
tigrisko98 <kostia.tigrisko@gmail.com>
2021-09-06 23:14:37 +0300

    implemented router

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,10 +1,14 @@
 <?php
+require_once('components/Parser.php');
 
 class SiteController
 {
     public function actionIndex()
     {
-        echo 'loh';
+        if (isset($_POST['submit'])) {
+            $parsedFile = Parser::parseFile($_FILES['text']['tmp_name']);
+        }
+        require_once('views/site/index.php');
         return true;
     }
 }
